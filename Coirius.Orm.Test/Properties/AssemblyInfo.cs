8fef8f4a157cf86e48b666ef8c2154f53ec99427
Vladyslav Chyzhevskyi <coirius@coirius.com>
2014-01-07 22:09:28 +0100

    created unit tests

---
+++ Coirius.Orm.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Coirius.Orm.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Coirius.Orm.Test")]
+[assembly: AssemblyCopyright("Copyright © Vladyslav Chyzhevskyi, 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("56528b49-9a22-4ee3-aabd-bc3b7207767a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
