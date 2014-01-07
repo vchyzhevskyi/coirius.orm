693fb9f132bc64ba31460202d3cc4224387ee87d
Vladyslav Chyzhevskyi <coirius@coirius.com>
2014-01-07 22:17:09 +0100

    added basic test for EqualExpression

---
+++ Coirius.Orm.Test/OrmExpressionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Coirius.Orm.Test
+{
+    [TestClass]
+    public class OrmExpressionTest
+    {
+        [TestMethod]
+        public void EqualExpressionTest()
+        {
+            OrmColumn column = new OrmColumn { Name = "Test" };
+            EqualExpression expr = new EqualExpression(column, "test");
+            Assert.AreEqual("(Test = test)", expr.ToString());
+            Assert.AreNotEqual("Test IN test", expr.ToString());
+
+            expr = new EqualExpression(column, 10);
+            Assert.AreEqual("(Test = 10)", expr.ToString());
+            Assert.AreNotEqual("Test IN 10", expr.ToString());
+        }
+    }
+}
