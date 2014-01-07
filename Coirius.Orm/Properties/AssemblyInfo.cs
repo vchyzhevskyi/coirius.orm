69ebe94f29ec31803408cf059a605878f837ca61
Vladyslav Chyzhevskyi <coirius@coirius.com>
2014-01-07 20:10:22 +0100

    created library project

---
+++ Coirius.Orm/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Coirius.Orm")]
+[assembly: AssemblyDescription("Object-Relational Mapper for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Coirius.Orm")]
+[assembly: AssemblyCopyright("Copyright © Vladyslav Chyzhevskyi, 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("187477c0-928d-4bb7-af7d-8dfa4b82b0fd")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
